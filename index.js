2266d6bc61975e33c4cf3df594f56a1fc5fdf297
aryan-srivastava1625 <ap9140@srmist.edu.in>
2025-06-12 17:36:31 +0530

    for different ids

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ const app = express();
 const PORT = 8000;
 
 //Routes
-app.get('/api/users',(req,res)=>{
-    return res.json(users);
-});
-
 
 app.get('/users',(req,res)=>{
     const html= `
@@ -19,6 +15,24 @@ app.get('/users',(req,res)=>{
     `;
     res.send(html);
 })
+
+//REST APIs
+app.get('/api/users',(req,res)=>{
+    return res.json(users);
+});
+
+//For id means agar hame id ke basis per person chaiye
+
+app.get('/api/users/:id',(req,res)=>{
+    const id=Number(req.params.id);
+    const user= users.find((user)=>user.id ===id);
+    return res.json(user);
+})
+
+
+
+
+
 
 
 app.listen(PORT,()=> console.log(`Server is running on port ${PORT}`));
